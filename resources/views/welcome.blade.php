609a0671bb5ba9a611abf6238b1a6e642feaaa06
jenniferyusiaa <arilhisyam247@gmail.com>
2024-12-05 02:50:25 +0700

    styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,6 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
     <style>
         /* ! tailwindcss v3.4.1 | MIT License | https://tailwindcss.com */
         *,
